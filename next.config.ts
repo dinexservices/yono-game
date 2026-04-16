9acd5cec2f441743e71ecb604e410053bf15307a
Abhishek Kumar <meaviishek@gmail.com>
2026-04-16 08:43:33 +0530

    ch

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ["www.allyonoogames.com", "allyonoogames.com"],
   images: {
     remotePatterns: [
       {
